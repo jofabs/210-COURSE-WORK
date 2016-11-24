1a4b9a456449971c6757dd060ca470b45dcb0e97
jofabs <fabiyij@uni.coventry.ac.uk>
2016-11-24 14:50:03 +0000

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,26 @@
+""" Task 1
+Write a function that randomly shuffles an array of integers
+and explain the rationale behind its implementation."""
+
+import random
+
+Num = [0,13,3,5,6,98]
+
+def randomRearrange(Num):
+    newList=[]
+    while Num != []:
+    #for i in Num:
+        i=random.choice((Num))
+        newList.append(i)
+        Num.remove(i)
+        print(i,Num,newList)
+    return newList
+
+print ("Original range of numbers are", Num)
+print("this is the new result", randomRearrange(Num))
+
+
+# ATTEMPT 1 Random.shuffle solution EASY STEP
+#theNum = ['0','13','3','5','6','98']
+#random.shuffle(theNum)
+#print (theNum)
---
+++ Task 3.py
@@ -0,0 +1,38 @@
+"""Question 3
+Write the pseudocode for a function which returns the highest perfect square
+which is less or equal to its parameter (a positive integer).
+Implement this in a programming language of your choice."""
+
+import math
+
+"""Using math
+#square root of the number and round it to the largest integer
+#value less than or equal to x: math.floor(x)"""
+
+paraNO = input("Give parametre: ")
+def perfectsquare(paraNO):
+    #Code below returns the floor of paraNO, largest integer < or = to paraNO
+    # and returns square root of parametre
+    paraN1 = math.floor(math.sqrt(int(paraNO)))
+    squarPARA = paraN1 * paraN1
+    print (squarPARA)
+
+perfectsquare(paraNO)
+
+
+"""
+PSEUDOCODE
+
+COLLECT USER INPUT
+
+DEFINE FUNCTION WITH ELEMENT:
+
+
+""""
+
+#""" Fuction which returns true if the parametre is a perfect square"""
+#n = int(input("Give parametre: "))
+#def perfectsquare(n):
+#    print (n % n**0.5 == 0)
+
+#perfectsquare(n)
