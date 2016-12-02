c602facc485213f1cf221b6334e2999a7511d1b0
jofabs <fabiyij@uni.coventry.ac.uk>
2016-12-02 17:18:15 +0000

    Update Task 2.py

--- Task 2.py
+++ Task 2.py
@@ -1,4 +1,4 @@
-"""TASK 1: Question 2
+"""TASK 2
 Count the number of trailing 0s (number of 0s at the end of the number)
 in a factorial number. Input: 5 Output: 1, Input: 10 Output: 2
 Hint: Count the prime factors of 5"""
@@ -16,5 +16,3 @@ def tailingZeros(f):#function defined containing users factorial number
 
 # elegant print statement
 print(str(f) + " has " + str(tailingZeros(f)) + " trailing zeros.")
-
-#log n or linear
