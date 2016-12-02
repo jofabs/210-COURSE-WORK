845c395ddb411fe2f544c146f964dbc19f3dfeb6
jofabs <fabiyij@uni.coventry.ac.uk>
2016-12-02 10:05:36 +0000

    Update Task 5.py

--- Task 5.py
+++ Task 5.py
@@ -8,43 +8,36 @@ INPUT(Matrix):
     B <- INPUT Matrix
     C <- INPUT Matrix
 
-ADDITION(B,C)
-    for a <- 0 to z
-        for l <- 0 to z
-            A[a][l] <- B[a][l] <- C[a][l]
-
-    return A
-
-MULTIPLICATION(A,m)
-    for a <-0 to z
-        for l <- 0 to z
-            A[a][l] <- ADD[a][l]*m
-
-    return A
-
-MATRIXMULTIPLICATION(B,C)
-    for a <-0 to z
-        for l <-0 to z
-            for e <- 0 to z
-                value <- B[a][e]*C[e][l]
-                total[a][l] <- total[a][l] + value
-
-    return total
-
 SUBTRACT(B,C):
     for a <- 0 to n:
         for l <- 0 to n:
             A[a][l] <- B[a][l] - C[a][l]
-
     return A
 
-TOTAL <- B*C + 2 (B+2)
-TOTALI <- MULTIPLYMATRIX(B,C)
-TOTALII <- ADD(B,C)
-TOTALIII <- SUBTRACT(B,C)
-TOTALIV <- MULTIPLICATION(A,2)
+ADDITION(B,C)
+    for a <- 0 to z
+        for l <- 0 to z
+            A[a][l] <- B[a][l] <- C[a][l]
+    return A
 
+MULTIPLICATION(A,m)
+    for a <-0 to m
+        for l <- 0 to m
+            A[a][l] <- ADD[a][l]*m
+    return A
 
+MATRIXMULTIPLICATION(B,C)
+    for a <-0 to z
+            for e <- 0 to z
+                power <- B[l][e]- C[e][l]
+                total[a][l] = total[a][l] + power
+    return total
+
+T.MATRIXMULTIPLICATION(B,C)
+T = B*C + 2 (B+2)
+T.ADD(B,C)
+T.MULTIPLICATION(A*2)
+T.SUBTRACT(B,C)
 
 
 #Runtime of BIG O is O(log n)
